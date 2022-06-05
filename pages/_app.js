d1c1cf418ce6f3debe7a3486ce62d1bcd156b611
Alberto Cházaro <shazarerom@gmail.com>
2022-06-04 19:24:55 -0500

    Creando state para el carrito de compras, agregarlos y evitar duplicados

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,35 @@
+import { useState } from 'react'
 import '../styles/normalize.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [carrito, setCarrito] = useState([])
+
+  const agregarCarrito = producto => {
+
+    //Identificar si un elemento ya está en el carrito utilizando el método some que nos retorna true o false
+    if (carrito.some(articulo => articulo.id === producto.id)) {
+      const carritoActualizado = carrito.map(articulo => {
+        if (articulo.id === producto.id) {
+          articulo.cantidad = producto.cantidad
+        }
+        return articulo
+      })
+      setCarrito(carritoActualizado)
+    } else {
+      setCarrito([...carrito, producto])
+    }
+
+  }
+
+  return (
+    <Component 
+      {...pageProps} 
+      carrito={carrito} //estado se extrae en ./carrito
+      agregarCarrito={agregarCarrito} //Función se extrae en ./guitarras/[url].js
+    />
+  )
 }
 
 export default MyApp
--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,10 +1,33 @@
+import { useState } from 'react'
 import Image from "next/image"
 import Layout from "../../components/Layout"
 import styles from "../../styles/Guitarra.module.css"
 
-const Producto = ({guitarra}) => {
+const Producto = ({guitarra, agregarCarrito}) => {
 
-    const {descripcion, imagen, nombre, precio} = guitarra[0]
+    const [cantidad, setCantidad] = useState(0)
+
+    const {descripcion, imagen, nombre, precio, id} = guitarra[0]
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        //Validar se este agregando un producto
+        if (cantidad < 1) {
+            alert('Cantidad no valida')
+            return
+        }
+
+        //Agregarlo al carrito
+        const guitarraSeleccionada = {
+            id,
+            imagen: process.env.NEXT_PUBLIC_API_URL+imagen.url,
+            nombre,
+            precio,
+            cantidad
+        }
+        agregarCarrito(guitarraSeleccionada)
+    }
 
     return (
         <Layout
@@ -22,9 +45,12 @@ const Producto = ({guitarra}) => {
                     <h3>{nombre}</h3>
                     <p className={styles.descripcion}>{descripcion}</p>
                     <p className={styles.precio}>${precio}</p>
-                    <form className={styles.formulario}>
+                    <form className={styles.formulario} onSubmit={handleSubmit}>
                         <label>Cantidad:</label>
-                        <select>
+                        <select
+                            value={cantidad}
+                            onChange={e => setCantidad(parseInt(e.target.value))}
+                        >
                             <option value={""}>-- Selecciones --</option>
                             <option value={"1"}>1</option>
                             <option value={"2"}>2</option>
